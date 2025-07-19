d16b31cf971836efafe5149c74bcd076afd1065f
monica <monica.njoki@student.moringaschool.com>
2025-07-19 16:48:52 +0300

    state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { useState } from "react"
 function Item({ name, category }) {
+  const [addToCart, setAddToCart] = useState(false)
+  function handleClick(){
+    setAddToCart((prevAddToCart) => !prevAddToCart)
+  }
   return (
-    <li className="">
+    <li className={addToCart ? "in-cart" : ""} data-testid = "item">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick}className={addToCart ? "remove" : "add"}>{addToCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const handleCategoryChange= (event) => {
+    setSelectedCategory(event.target.value)
+  }
+  const selectedItems = items.filter((item) => {return selectedCategory === "All" || item.category === selectedCategory
+
+  })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange = {handleCategoryChange} value = {selectedCategory}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +21,13 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {selectedItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
   );
-}
+  }
+  
 
 export default ShoppingList;
